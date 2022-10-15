7e33071fdba47da89233621ae6d710600e898041
Your Name <rodrigo.santos1994a@gmail.com>
2022-10-14 21:32:59 -0300

    First Stage

---
+++ portfolio/views.py
@@ -0,0 +1,7 @@
+import re
+from django.shortcuts import render
+from .models import Project
+# Create your views here.
+def home(request):
+    projects=Project.objects.all()
+    return render(request, "home.html", {"projects":projects})
