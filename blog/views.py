a3ebb15b9a6aa9d98ecb26626b9101e42bf9be3c
Rodrigo Damian Santos <106984817+RDSDeveloper@users.noreply.github.com>
2022-10-19 15:18:54 -0300

    Cambios EZE

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from portfolio.models import Project
+
 
 def render_posts(request):
-    return render(request, "post.html")
+    projects=Project.objects.all()
+
+    return render(request, "post.html", {"projects":projects})
--- django_portfolio_core/urls.py
+++ django_portfolio_core/urls.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+
+
 from portfolio import views
-from blog import urls
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.home, name="home"),
-    path("/blog", views.home, name="home"),
+    path("blog/", include("blog.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
