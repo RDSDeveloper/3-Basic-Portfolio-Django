4cbd41f5255ce9e519701339979daac350a0f480
Your Name <rodrigo.santos1994a@gmail.com>
2023-01-14 19:22:20 -0300

    Fusion?

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,6 +5,6 @@ from django.db.models.fields.files import ImageField
 
 class Project(models.Model):
     title = CharField(max_length=100)
-    description = CharField(max_length=250)
+    description = CharField(max_length=1500)
     image = ImageField(upload_to="portfolio/images/")
     url = URLField(blank=True)
