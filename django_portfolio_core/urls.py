3f841ae98c9af6f2a87be3e46c99305b54b4a27b
Your Name <rodrigo.santos1994a@gmail.com>
2023-04-12 10:06:29 -0300

    MyPortfolioV1

--- django_portfolio_core/urls.py
+++ django_portfolio_core/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
-
 
 from portfolio import views
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -14,3 +13,8 @@ urlpatterns = [
     path("blog/", include("blog.urls")),
 ]
 
+
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
